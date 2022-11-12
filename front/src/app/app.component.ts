22c12ea8b7a15b3c1ae36a5acea9258010894ebe
ryohei-ochi-fr <ryohei.ochi@futurerays.biz>
2022-11-13 07:59:41 +0900

    add socket.io C/S ok

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -24,5 +24,16 @@ export class AppComponent {
       ],
       minDimensions: [10, 10]
     });
+
+    // 外部のjsを読み込む場合
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js';
+
+    const div = document.getElementById('script');
+    if (div != null){
+      div.insertAdjacentElement('afterend', script);
+    }
+
   }
 }
